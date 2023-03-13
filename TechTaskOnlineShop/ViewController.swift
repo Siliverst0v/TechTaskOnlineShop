6601f14dc0b7fc48d3b7ff094cfa216ae08f3840
Anatoliy <94259002+Siliverst0v@users.noreply.github.com>
2023-03-13 19:58:27 +0300

    Initial Commit

---
+++ TechTaskOnlineShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TechTaskOnlineShop
+//
+//  Created by Анатолий Силиверстов on 13.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
